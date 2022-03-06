2282970b39641a16304d7de87da9ca4a1d72b80f
giangbt-2949 <SUN-ASTERISK\bui.truong.giang@b122949-pc.sun-asterisk.com>
2022-03-06 18:46:55 +0700

    truonggiang-responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,19 +33,19 @@ $(function () {
   //=====
 });
 //mouse move
-document.addEventListener('mousemove', function (e) {
-  var body = document.querySelector('body');
-  var heart = document.createElement('div');
-  heart.className = 'love';
-  var x = e.offsetX;
-  var y = e.offsetY;
-  heart.style.left = x + 'px';
-  heart.style.top = y + 'px';
-  var size = Math.random() * 100;
-  heart.style.width = 5 + size + 'px';
-  heart.style.height = 5 + size + 'px';
-  body.appendChild(heart);
-  setTimeout(function () {
-    heart, remove();
-  }, 2);
-});
+// document.addEventListener('mousemove', function (e) {
+//   var body = document.querySelector('body');
+//   var heart = document.createElement('div');
+//   heart.className = 'love';
+//   var x = e.offsetX;
+//   var y = e.offsetY;
+//   heart.style.left = x + 'px';
+//   heart.style.top = y + 'px';
+//   var size = Math.random() * 100;
+//   heart.style.width = 5 + size + 'px';
+//   heart.style.height = 5 + size + 'px';
+//   body.appendChild(heart);
+//   setTimeout(function () {
+//     heart, remove();
+//   }, 2);
+// });
